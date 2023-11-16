cbc4298f2b1778486a4f22df2d68a5b62b2193a1
Debobroto Das <debojitdas0707@gmail.com>
2023-11-17 01:00:41 +0530

    bug fixes

--- app/src/main/java/com/example/tweetsy3/viewmodels/DetailViewModel.kt
+++ app/src/main/java/com/example/tweetsy3/viewmodels/DetailViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.tweetsy3.viewmodels
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.tweetsy3.model.TweetsListItem
@@ -10,14 +11,17 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class DetailViewModel @Inject constructor(private val repository: TweetsRepository) : ViewModel() {
+class DetailViewModel @Inject constructor(
+    private val repository: TweetsRepository,
+    private val savedStateHandle: SavedStateHandle) : ViewModel() {
 
     val tweets: StateFlow<List<TweetsListItem>>
         get() = repository.tweets
 
     init {
         viewModelScope.launch {
-            repository.getTweets("motivation")
+            val category = savedStateHandle.get<String>("category")?:"motivation"
+            repository.getTweets(category)
         }
     }
 }
